ef16ed02824cafad44549a1115e328018e460453
Julian Jandl <julian@jandl.wien>
2022-04-05 10:41:59 +0200

    michael ex-3 complete

--- projects/movies/src/app/app.component.ts
+++ projects/movies/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
+import { MovieModel } from './movie-card/movie-model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  movie: MovieModel = {
+    id: '100',
+    title: 'Turning Red',
+    poster_path: '/qsdjk9oAKSQMWs0Vt5Pyfh6O4GZ.jpg',
+    vote_average: 8,
+  };
 
-  constructor(
-  ) { }
+  constructor() {}
 
+  navToDetail(movie: MovieModel): void {
+    console.log('selected', movie);
+    console.log(movie);
+  }
 }
--- projects/movies/src/app/app.module.ts
+++ projects/movies/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppShellModule } from './app-shell/app-shell.module';
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppShellModule
-  ],
+  declarations: [AppComponent, MovieCardComponent],
+  imports: [BrowserModule, AppShellModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
