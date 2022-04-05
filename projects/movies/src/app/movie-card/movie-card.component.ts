b1146dd72394405f9f63b09e4b330d9fb230eefe
Julian Jandl <julian@jandl.wien>
2022-04-05 10:40:46 +0200

    exercise 3 complete for michaeal

---
+++ projects/movies/src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MovieModel } from './movie-model';
+
+type BooleanInputType = string | boolean | null | undefined;
+
+@Component({
+  selector: 'movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss'],
+})
+export class MovieCardComponent implements OnInit {
+  @Input() movie: MovieModel;
+
+  @Input()
+  set disabled(value: BooleanInputType) {
+    if (value === '' || value === true) {
+      // disabled
+    }
+  }
+
+  @Output() selected = new EventEmitter<MovieModel>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  movieClicked() {
+    this.selected.emit(this.movie);
+  }
+}
