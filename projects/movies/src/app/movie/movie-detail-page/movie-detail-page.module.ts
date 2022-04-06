98b74608593bff65f34b094cc97050cea56b6198
Julian Jandl <julian@jandl.wien>
2022-04-06 13:38:16 +0200

    wip detail

--- projects/movies/src/app/movie/movie-detail-page/movie-detail-page.module.ts
+++ projects/movies/src/app/movie/movie-detail-page/movie-detail-page.module.ts
@@ -1,5 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailGridModule } from '../../ui/component/detail-grid/detail-grid.module';
+import { SvgIconModule } from '../../ui/component/icons/icon.module';
+import { StarRatingModule } from '../../ui/pattern/star-rating/star-rating.module';
+import { MovieModule } from '../movie.module';
 import { MovieDetailPageComponent } from './movie-detail-page.component';
 
 const routes: Routes = [
@@ -11,6 +16,13 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [MovieDetailPageComponent],
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    RouterModule.forChild(routes),
+    SvgIconModule,
+    CommonModule,
+    MovieModule,
+    StarRatingModule,
+    DetailGridModule,
+  ],
 })
 export class MovieDetailPageModule {}
