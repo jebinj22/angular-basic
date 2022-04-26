616a23dd02d1342ee1ce77e9939c2422e2e3ec28
Michael Hladky <michael@hladky.at>
2022-04-26 11:01:51 +0200

    first pitfalls, subscripe, side effects

--- projects/movies/src/app/movie/movie-list-page/movie-list-page.component.ts
+++ projects/movies/src/app/movie/movie-list-page/movie-list-page.component.ts
@@ -1,31 +1,79 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, interval, map, Observable, of, Subject, Subscription, switchMap, takeUntil, tap } from 'rxjs';
 import { MovieModel } from '../movie-model';
 import { MovieService } from '../movie.service';
+import { RxEffects } from '@rx-angular/state/effects';
 
 @Component({
   selector: 'movie-list-page',
-  templateUrl: './movie-list-page.component.html',
-  styleUrls: ['./movie-list-page.component.scss'],
-})
-export class MovieListPageComponent implements OnInit, OnDestroy {
-  movies$: Observable<{ results: MovieModel[] }>;
+  template: `
+    <input name="search">
 
-  private sub: Subscription;
+    <ng-container *ngIf="(movies$ | async) as movieResponse; else: loader">
+
+      <movie-list
+        *ngIf="movieResponse.results?.length > 0; else: elseTmpl"
+        [movies]="movieResponse.results">
+      </movie-list>
+
+      <ng-template #elseTmpl>
+        <div>Sorry, nothing found bra!</div>
+      </ng-template>
+
+
+    </ng-container>
+
+
+    <ng-template #loader>
+      <div class="loader"></div>
+    </ng-template>
+  `,
+  providers: [RxEffects]
+})
+export class MovieListPageComponent {
+  movies$: Observable<{ results: MovieModel[] }> = this.activatedRoute.params.pipe(
+    switchMap(params => this.movieService.getMovieList(params.category))
+  );
+
+  /*
+    state$ = new BehaviorSubject({ list: [1, 2, 43] });
+    selection$ = this.state$.pipe(
+      tap(console.log),
+      // filter(v => v !== undefined),
+      //  distinctUntilKeyChanged('list'),
+      map((s) => ({ list: s.list, count: s.list.length }))
+      //      shareReplay(1)
+    );
+  */
+
 
   constructor(
+    private rxEffects: RxEffects,
     private activatedRoute: ActivatedRoute,
     private movieService: MovieService
-  ) {}
-
-  ngOnInit() {
-    this.sub = this.activatedRoute.params.subscribe((params) => {
+  ) {
+    /*const trigger$ = activatedRoute.params;
+    const effect = (params) => {
+      // no side effect!!
       this.movies$ = this.movieService.getMovieList(params.category);
-    });
+    };
+    this.rxEffects.register(trigger$.pipe(
+      switchMap(params => this.movieService.getMovieList(params.category))
+    ));
+    */
+    /*
+     selection$.subscribe((v) => console.log('new', v));
+  selection$.subscribe((v) => console.log('new', v));
+  selection$.subscribe((v) => console.log('new', v));
+  selection$.subscribe((v) => console.log('new', v));
+  selection$.subscribe((v) => console.log('new', v));
+
+  selection$.subscribe((v) => console.log('new', v));
+  selection$.subscribe((v) => console.log('new', v));
+  selection$.subscribe((v) => console.log('new', v));
+    * */
+
   }
 
-  ngOnDestroy() {
-    this.sub?.unsubscribe();
-  }
 }
